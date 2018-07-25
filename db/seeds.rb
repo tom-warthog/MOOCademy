12755557c6d226eeb9cdbf0599a824e2b7062a3c
Unknown <nicolas.bertin0@gmail.com>
2018-07-25 20:32:09 +0200

    minor fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'faker'
 
 10.times do
@@ -13,5 +5,5 @@ require 'faker'
 end
 
 50.times do
-  lesson = Lesson.create(title: Faker::BossaNova.artist, body: Faker::BossaNova.song)
+  lesson = Lesson.create(title: Faker::BossaNova.artist, body: Faker::BossaNova.song, course_id: rand(1..10))
 end
