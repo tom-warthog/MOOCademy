9eed009a390db1a9a033587fae6080bf4f260625
Unknown <nicolas.bertin0@gmail.com>
2018-07-25 16:46:21 +0200

    final commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  course = Course.create(title: Faker::Cat.name, description: Faker::Cat.registry)
+end
+
+50.times do
+  lesson = Lesson.create(title: Faker::BossaNova.artist, body: Faker::BossaNova.song)
+end
